f6d6f01ebd8528923b713975229433c4c0922700
HanSeungYoon <104001741+Yoon0717@users.noreply.github.com>
2025-04-04 13:34:16 +0900

    [Silver I] Title: IOIOI, Time: 36 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,25 @@
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+n = int(input()) # Pn의 n
+m = int(input()) # sen의 길이
+sen = input()
+
+length = 0 # IOI의 갯수
+count = 0 # Pn의 갯수
+
+i = 0
+while i < (m-1):
+    if sen[i-1] == 'I' and sen[i] == 'O' and sen[i+1] == 'I':
+        length += 1
+        i += 2
+    else:
+        if length-n+1 > 0:
+            count += (length-n+1)
+        i += 1
+        length = 0
+
+if length-n+1 > 0:
+     count += (length-n+1)
+     
+print(count)
