2e4ffd6b8e95887839ebfee18dfa5619fd9370f8
HanSeungYoon <104001741+Yoon0717@users.noreply.github.com>
2025-07-31 17:33:23 +0900

    [Silver I] Title: 경로 찾기, Time: 152 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,28 +1,15 @@
 import sys
-from collections import deque
-input = lambda : sys.stdin.readline().rstrip()
-
-def dfs(k, t):
-  visited.append(t)
-  for i in point[t]:
-    line[k][i] = 1
-    if i not in visited:
-      dfs(k, i)
+input = sys.stdin.readline
 
 n = int(input())
-point = {i:[] for i in range(n)}
+graph = [list(map(int, input().split())) for _ in range(n)] # 이미 갈 수 있는 길을 내포하므로, 이 내용을 활용하는게 좋음
 
-for i in range(n):
-  k = list(map(int, input().split()))
-  for j in range(len(k)):
-    if k[j] == 1:
-      point[i].append(j)
+for k in range(n):
+  for i in range(n):
+    if graph[i][k]: # i->k가 된다면
+        for j in range(n):
+          if graph[k][j]: # k->i가 된다면
+            graph[i][j] = 1 # i->j가 된다
 
-line = [[0 for _ in range(n)] for _ in range(n)]
-
-for i in range(n):
-  visited = []
-  dfs(i, i)
-
-for l in line:
-  print(*l)
+for g in graph:
+  print(*g)
