940afbab2f1bc27f3dc9760fb499e72485fe51a0
HanSeungYoon <104001741+Yoon0717@users.noreply.github.com>
2025-09-12 18:27:56 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,18 @@
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+n ,m = map(int, input().split())
+seq = []
+
+def dfs(start):
+  if len(seq) == m:
+    print(' '.join(map(str, seq)))
+    return
+  
+  for i in range(start, n+1):
+    if i not in seq:
+      seq.append(i)
+      dfs(i+1)
+      seq.pop()
+
+dfs(1)
