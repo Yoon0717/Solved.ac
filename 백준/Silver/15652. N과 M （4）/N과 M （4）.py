b2a43c5ec04b778492bb26bf7b93e1b75937480a
HanSeungYoon <104001741+Yoon0717@users.noreply.github.com>
2025-09-13 16:40:06 +0900

    [Silver III] Title: N과 M (4), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,16 @@
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+n, m = map(int, input().split())
+seq = []
+
+def dfs(start):
+  if len(seq) == m:
+    print(' '.join(map(str,seq)))
+    return
+  for i in range(start, n+1):
+    seq.append(i)
+    dfs(i)
+    seq.pop()
+
+dfs(1)
