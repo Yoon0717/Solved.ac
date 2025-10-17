194ad2d955440519608af109b3b62cc488c27cac
HanSeungYoon <104001741+Yoon0717@users.noreply.github.com>
2025-10-17 16:36:49 +0900

    [Gold IV] Title: 플로이드, Time: 480 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,42 +1,76 @@
+### 플로이드 워셜 ###
+
 import sys
-import heapq
 input = lambda : sys.stdin.readline().rstrip()
 
 n = int(input())
 m = int(input())
 
 INF = float('inf')
-ans = [[INF]*(n+1) for _ in range(n+1)]
-bus = {i:[] for i in range(1, n+1)}
+graph = [[INF]*(n+1) for _ in range(n+1)]
+for i in range(1,n+1):
+  for j in range(1,n+1):
+    if i == j:
+      graph[i][j] = 0
 
 for _ in range(m):
-  start, end, cost = map(int, input().split())
-  bus[start].append((end, cost))
-  ans[start][end] = min(ans[start][end], cost)
+    a, b, c = map(int, sys.stdin.readline().split())
+    graph[a][b] = min(c, graph[a][b])
 
-for start in range(1, n+1):
-  heap = []
-  for k in bus[start]: # i:start, k:(cost, end)
-    end, cost = k
-    if ans[start][end] >= cost:
-      heapq.heappush(heap, (cost, end))
-  
-  while heap:
-    cost, current = heapq.heappop(heap)
-    if cost > ans[start][current]:
-      continue
-    
-    ans[start][current] = min(ans[start][current], cost)
-    for k in bus[current]:
-      next, next_cost = k
-      next_cost += cost
-      if ans[start][next] > next_cost:
-        heapq.heappush(heap, (next_cost, next))
+for k in range(1, n+1):
+  for i in range(1, n+1):
+    for j in range(1, n+1):
+      graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
 
 for i in range(1, n+1):
   for j in range(1, n+1):
-    if ans[i][j] == INF or i==j:
+    if graph[i][j] == INF:
       print(0, end=' ')
     else:
-      print(ans[i][j], end=' ')
+      print(graph[i][j], end=' ')
   print()
+
+### 다익스트라 ###
+
+# import sys
+# import heapq
+# input = lambda : sys.stdin.readline().rstrip()
+
+# n = int(input())
+# m = int(input())
+
+# INF = float('inf')
+# ans = [[INF]*(n+1) for _ in range(n+1)]
+# bus = {i:[] for i in range(1, n+1)}
+
+# for _ in range(m):
+#   start, end, cost = map(int, input().split())
+#   bus[start].append((end, cost))
+#   ans[start][end] = min(ans[start][end], cost)
+
+# for start in range(1, n+1):
+#   heap = []
+#   for k in bus[start]: # i:start, k:(cost, end)
+#     end, cost = k
+#     if ans[start][end] >= cost:
+#       heapq.heappush(heap, (cost, end))
+  
+#   while heap:
+#     cost, current = heapq.heappop(heap)
+#     if cost > ans[start][current]:
+#       continue
+    
+#     ans[start][current] = min(ans[start][current], cost)
+#     for k in bus[current]:
+#       next, next_cost = k
+#       next_cost += cost
+#       if ans[start][next] > next_cost:
+#         heapq.heappush(heap, (next_cost, next))
+
+# for i in range(1, n+1):
+#   for j in range(1, n+1):
+#     if ans[i][j] == INF or i==j:
+#       print(0, end=' ')
+#     else:
+#       print(ans[i][j], end=' ')
+#   print()
